dbea761395dd27eb0bbd909b92aacf3dca407c63
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2024-01-17 12:54:36 -0500

    Update addresses.

--- config/testnet.go
+++ config/testnet.go
@@ -8,8 +8,8 @@ type Contracts struct {
 }
 
 var TestnetContracts = Contracts{
-	BidderRegistry:         "0x62197Abd7672925c7606Bdf9931e42baCa6619AD",
+	BidderRegistry:         "0xb1692ab820BF196d54e21Fbb1607931558B1d077",
 	ProviderRegistry:       "0xeA73E67c2E34C4E02A2f3c5D416F59B76e7617fC",
-	PreconfCommitmentStore: "0x39a591256f51a36e83F63252E727F5a5d8711236",
-	Oracle:                 "0x8dfFDA0B27E848c69D92fF1F3E02c274a5801646",
+	PreconfCommitmentStore: "0x8C8571F28a90813f8C468E5dbCF3B53c94aCE580",
+	Oracle:                 "0x8a965147eD06B3faF672a71bED3689161B8109c3",
 }
